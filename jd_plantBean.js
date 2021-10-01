65d15fe2a01f9bf7f4f782bc820bd3a689368e84
KingRan <754879133@qq.com>
2021-10-02 01:13:37 +0800

    内部互助

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -22,7 +22,7 @@ cron "1 7-21/2 * * *" script-path=https://raw.githubusercontent.com/KingRan/JDJB
 京东种豆得豆 = type=cron,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_plantBean.js, cronexpr="1 7-21/2 * * *", timeout=3600, enable=true
 
 */
-let sc = require("./cdle_carry_share_code.js")
+let sc = require("./KingRan_JDJB_share_code.js")
 const $ = new Env('京东种豆得豆');
 //Node.js用户请在jdCookie.js处填写京东ck;
 //ios等软件用户直接用NobyDa的jd cookie
