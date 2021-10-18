04321b42e6228ac002cefbefdeac9b5c6ba35d80
KingRan <754879133@qq.com>
2021-10-19 01:04:59 +0800

    更新助力逻辑

--- jd_cfd.js
+++ jd_cfd.js
@@ -1081,7 +1081,7 @@ async function getBuildInfo(body, buildList, type = true) {
             const body = `strBuildIndex=${data.strBuildIndex}&dwType=1`
             let collectCoinRes = await collectCoin(body)
             console.log(`【${buildNmae}】收集${collectCoinRes.ddwCoin}金币`)
-            await $.wait(2000)
+            await $.wait(3000)
             await getUserInfo(false)
             console.log(`升级建筑`)
             console.log(`【${buildNmae}】当前等级：${buildList.dwLvl}`)
@@ -1089,6 +1089,7 @@ async function getBuildInfo(body, buildList, type = true) {
             if(data.dwCanLvlUp > 0 && $.info.ddwCoinBalance >= (data.ddwNextLvlCostCoin * 3)) {
               console.log(`【${buildNmae}】满足升级条件，开始升级`)
               const body = `ddwCostCoin=${data.ddwNextLvlCostCoin}&strBuildIndex=${data.strBuildIndex}`
+			  await $.wait(2000)
               let buildLvlUpRes = await buildLvlUp(body)
               if (buildLvlUpRes.iRet === 0) {
                 console.log(`【${buildNmae}】升级成功：获得${data.ddwLvlRich}财富\n`)
@@ -1174,18 +1175,21 @@ function helpByStage(shareCodes) {
           console.log(`${$.name} helpbystage API请求失败，请检查网路重试`)
         } else {
           data = JSON.parse(data);
-          if (data.iRet === 0 || data.sErrMsg === 'success') {
+          if (data.iRet === 0 && data.sErrMsg === 'success') {
             console.log(`助力成功：获得${data.Data.GuestPrizeInfo.strPrizeName}`)
-          } else if (data.iRet === 2232 || data.sErrMsg === '今日助力次数达到上限，明天再来帮忙吧~') {
+          } else if (data.iRet === 2232 && data.sErrMsg === '今日助力次数达到上限，明天再来帮忙吧~') {
             console.log(`助力失败：${data.sErrMsg}`)
             $.canHelp = false
-          } else if (data.iRet === 9999 || data.sErrMsg === '您还没有登录，请先登录哦~') {
+          } else if (data.iRet === 2232 && data.sErrMsg === '分享链接已过期') {
+            console.log(`助力失败：${data.sErrMsg}`)
+            $.delcode = true
+          } else if (data.iRet === 9999 && data.sErrMsg === '您还没有登录，请先登录哦~') {
             console.log(`助力失败：${data.sErrMsg}`)
             $.canHelp = false
-          } else if (data.iRet === 2229 || data.sErrMsg === '助力失败啦~') {
+          } else if (data.iRet === 2229 && data.sErrMsg === '助力失败啦~') {
             console.log(`助力失败：您的账号已黑`)
             $.canHelp = false
-          } else if (data.iRet === 2190 || data.sErrMsg === '达到助力上限') {
+          } else if (data.iRet === 2190 && data.sErrMsg === '达到助力上限') {
             console.log(`助力失败：${data.sErrMsg}`)
             $.delcode = true
           } else {
