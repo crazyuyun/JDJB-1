bb14d97338641396b0221306e5ec352d0ed90c57
KingRan <754879133@qq.com>
2021-10-26 23:15:25 +0800

    修正

--- jd_cart_remove.js
+++ jd_cart_remove.js
@@ -42,7 +42,7 @@ $.keywordsNum = 0;
                 "open-url": "https://bean.m.jd.com/bean/signIndex.action"
             });
         }
-        for(let i = 7; i < cookiesArr.length; i++){
+        for(let i = 0; i < cookiesArr.length; i++){
             if(cookiesArr[i]){
                 cookie = cookiesArr[i];
                 $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
