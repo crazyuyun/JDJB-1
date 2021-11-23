628aa741229c1b08b2921a7d3a451cf5f5240fbb
KingRan <754879133@qq.com>
2021-11-24 00:37:14 +0800

    更新

--- jdCookie.js
+++ jdCookie.js
@@ -88,7 +88,7 @@ function SetShareCodesEnv(nameConfig = "", envName = "") {
   let shareCodesStr = shareCodes.join('&')
   process.env[envName] = shareCodesStr
 
-  console.info(`【风之凌殇】 友情提示：为避免ck超过45以上时，互助码环境变量过大而导致调用一些系统命令（如date/cat）时报 Argument list too long，改为在nodejs中设置 ${nameConfig} 的 互助码环境变量 ${envName}，共计 ${totalCodeCount} 组互助码，总大小为 ${shareCodesStr.length}`)
+  console.info(`友情提示：为避免ck超过45以上时，互助码环境变量过大而导致调用一些系统命令（如date/cat）时报 Argument list too long，改为在nodejs中设置 ${nameConfig} 的 互助码环境变量 ${envName}，共计 ${totalCodeCount} 组互助码，总大小为 ${shareCodesStr.length}`)
 }
 
 // 若在task_before.sh 中设置了要设置互助码环境变量的活动名称和环境变量名称信息，则在nodejs中处理，供活动使用
@@ -97,6 +97,6 @@ let envName = process.env.ShareCodeEnvName
 if (nameConfig && envName) {
   SetShareCodesEnv(nameConfig, envName)
 } else {
-  console.debug(`【风之凌殇】 友情提示：当前未设置 ShareCodeConfigName 或 ShareCodeEnvName 环境变量，将不会尝试在nodejs中生成互助码的环境变量。ps: 两个值目前分别为 ${nameConfig} ${envName}`)
+  console.debug(`友情提示：当前未设置 ShareCodeConfigName 或 ShareCodeEnvName 环境变量，将不会尝试在nodejs中生成互助码的环境变量。ps: 两个值目前分别为 ${nameConfig} ${envName}`)
   console.debug(`如果当前活动并没有互助内容，请无视上面这句话`)
 }
